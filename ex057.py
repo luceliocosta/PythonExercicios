7b291650541345ba7f59753a8162a7cf29e713c2
lucelio-costa <luceliocosta1994@hotmail.com>
2022-09-02 15:51:23 +0100

    Update ex057.py

--- ex057.py
+++ ex057.py
@@ -1,1 +1,19 @@
 #DESAFIO 057
+#: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça
+# a digitação novamente até ter um valor correto.
+
+'''
+sexo = ''
+while sexo != 'M' and sexo != 'F':
+    sexo = str(input('Informe o seu sexo: [M/F] ')).upper()
+    if sexo != 'M' and sexo != 'F':
+        print('Valor inválido. Digite novamente!')
+print(f'Sexo {sexo} registdo com sucesso!')
+'''
+
+sexo = str(input('Informe o seu sexo: [M/F] ')).strip().upper()[0]
+while sexo not in 'MF':
+    sexo = str(input('Valor inválido. Digite Novamente: [M/F] '))
+
+
+
